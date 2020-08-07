cca31b43a1e3e5c7a577f06d1e7bd9681b7b6fc6
foodisgood-glitch <69075108+foodisgood-glitch@users.noreply.github.com>
2020-08-08 08:40:35 +1000

    Add files via upload

---
+++ hub.lua
@@ -0,0 +1,11 @@
+local place = game.PlaceId
+
+if place == 5266363557 then -- https://www.roblox.com/games/5266363557/
+print("Loaded Combo Clickers GUI")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Combo%20Clickers%20script.lua"))()
+else
+if place == 5461749397 then -- https://www.roblox.com/games/5461749397/
+print("Loaded key ganme thing GUI")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Key%20Champions.lua"))()
+end
+end  
