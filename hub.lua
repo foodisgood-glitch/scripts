49346f84095cccc72cf207c365b0e44885dc79e9
foodisgood-glitch <69075108+foodisgood-glitch@users.noreply.github.com>
2020-08-08 08:41:11 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -8,4 +8,3 @@ if place == 5461749397 then -- https://www.roblox.com/games/5461749397/
 print("Loaded key ganme thing GUI")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Key%20Champions.lua"))()
 end
-end  
