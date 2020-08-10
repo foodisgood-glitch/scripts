2f15273f9bab5bca6dcd03af73894cbb82992c2e
foodisgood-glitch <69075108+foodisgood-glitch@users.noreply.github.com>
2020-08-10 17:35:12 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -4,7 +4,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scr
 end)
 if place == 5266363557 then -- https://www.roblox.com/games/5266363557/
 print("Loaded Combo Clickers GUI")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Combo%20Clickers%20script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Key%20Champions.lua"))()
 else
 if place == 5461749397 then -- https://www.roblox.com/games/5461749397/
 print("Loaded key ganme thing GUI")
