e30e68dc3b7bb1c7508b44c804bced78640cdff0
foodisgood-glitch <69075108+foodisgood-glitch@users.noreply.github.com>
2020-08-11 20:21:31 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,6 +1,6 @@
 local place = game.PlaceId
 spawn(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/intro", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/intro"))()
 end)
 if place == 5266363557 then -- https://www.roblox.com/games/5266363557/
 print("Loaded Combo Clickers GUI")
