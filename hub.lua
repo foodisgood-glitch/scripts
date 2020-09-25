930f8bea87cdc05f0c1c6463cabba8c651680ab1
foodisgood-glitch <69075108+foodisgood-glitch@users.noreply.github.com>
2020-09-25 17:48:44 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -10,13 +10,18 @@ if place == 5461749397 then -- https://www.roblox.com/games/5461749397/
 print("Loaded key ganme thing GUI")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Key%20Champions.lua"))()
 else
-if place == 5416606558 then -- https://www.roblox.com/games/5416606558/NEW-CLICKING-LEGENDS
+if place == 5416606558 then -- https://www.roblox.com/games/5416606558/
 print("Loaded clicking legends thing GUI")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/clicking%20legends%20script.lua"))()
 else
-if place == 5604707810 then -- https://www.roblox.com/games/5604707810/BOSS-Tree-Harvest-Simulator
+if place == 5604707810 then -- https://www.roblox.com/games/5604707810/
 print("Loaded Tree Harvest Simulator thing GUI")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Tree%20Harvest%20Simulator"))()
+else
+if place == 5641449689 then -- https://www.roblox.com/games/5641449689/
+print("Loaded Clicking 20Fantasy thing GUI")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgood-glitch/scripts/master/Clicking%20Fantasy%20script.lua"))()
+end
 end
 end
 end
