7b065e12ef7bcdf8bf46375a8413ea5e23e00336
foodisgod <69075108+foodisgod@users.noreply.github.com>
2021-03-30 18:40:19 +1000

    Update and rename new to Wack A Mole Simulator.lua

---
+++ Wack A Mole Simulator.lua
@@ -0,0 +1,63 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local venyx = library.new("Wack A Mole Simulator", 5013109572)
+
+-- themes
+local themes = {
+Background = Color3.fromRGB(24, 24, 24),
+Glow = Color3.fromRGB(0, 0, 0),
+Accent = Color3.fromRGB(10, 10, 10),
+LightContrast = Color3.fromRGB(20, 20, 20),
+DarkContrast = Color3.fromRGB(14, 14, 14),  
+TextColor = Color3.fromRGB(255, 255, 255)
+}
+
+-- first page
+local page = venyx:addPage("Main", 5012544693)
+local section1 = page:addSection("Section 1")
+local section2 = page:addSection("Section 2")
+
+section1:addToggle("Attack and Sell", nil, function(value)
+    _G.click = value
+    while wait() and _G.click do
+        game:GetService("ReplicatedStorage").Functions.Attack:InvokeServer()
+        game:GetService("ReplicatedStorage").Events.Sell:FireServer("Scorching Desert")
+        game:GetService("ReplicatedStorage").Events.Sell:FireServer("Forest Of Beginnings")
+    end
+end)
+
+
+if focusLost then
+venyx:Notify("Title", value)
+end
+
+section2:addToggle("Auto Egg", nil, function(value)
+    _G.Egg = value
+end)
+
+
+section2:addDropdown("Dropdown", {"Starter Egg", "Epic Egg", "Sand Egg", "Desert Egg"}, function(text)
+while wait() and _G.Egg do
+        game:GetService("ReplicatedStorage").Functions.Hatch:InvokeServer(text)
+    end
+end)
+
+
+
+section2:addSlider("speed", 16, 0, 100, function(value)
+game.Workspace.LocalPlayer.Humanoid.WalkSpeed = value
+end)
+
+section2:addButton("Button")
+
+-- second page
+local theme = venyx:addPage("Theme", 5012544693)
+local colors = theme:addSection("Colors")
+
+for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
+colors:addColorPicker(theme, color, function(color3)
+venyx:setTheme(theme, color3)
+end)
+end
+
+-- load
+venyx:SelectPage(venyx.pages[1], true)
